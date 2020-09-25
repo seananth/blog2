caede9c67babec2856e535f78ea484896c80e747
seananth <seansafstrom@gmail.com>
2020-09-25 15:49:14 +0200

    added edit and destroy, created form partial

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,35 @@
 class ArticlesController < ApplicationController
+    include ArticlesHelper
 
-def index
-@articles = Article.all.order(created_at: :desc)
+    def index
+    @articles = Article.all.order(created_at: :desc)
+    end
+
+    def show
+    @article = Article.find(params[:id])
+    end
+
+    def new
+    @article = Article.new
+    end
+
+    def create
+        @article = Article.new(article_params)
+        @article.save
+
+        redirect_to article_path(@article)
+    end
+
+    def destroy
+        @article = Article.find(params[:id]).destroy
+
+        redirect_to action: 'index'
+    end
+
+    def edit
+        @article = Article.find(params[:id])
+    end
+
 end
 
-def show
- @article = Article.find(params[:id])
-end
-
-end
+#Deletuing articles
