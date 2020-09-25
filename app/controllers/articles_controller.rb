472fa63fd0264feb6d3610386064d3c38417a409
seananth <seansafstrom@gmail.com>
2020-09-25 16:19:45 +0200

    added action notices

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,18 +16,28 @@ class ArticlesController < ApplicationController
     def create
         @article = Article.new(article_params)
         @article.save
+        flash.notice = "Successfully posted '#{@article.title}'"
 
         redirect_to article_path(@article)
     end
 
     def destroy
         @article = Article.find(params[:id]).destroy
+        flash.notice = "Article deleted"
 
         redirect_to action: 'index'
     end
 
     def edit
         @article = Article.find(params[:id])
+    end
+
+    def update
+        @article = Article.find(params[:id])
+        @article.update(article_params)
+        flash.notice = "Article '#{@article.title}' Updated!"
+
+        redirect_to article_path(@article)
     end
 
 end
